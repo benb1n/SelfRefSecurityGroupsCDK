d7a435428e7140485261b451893001384e52b503
Ben Ellis <ben@element11.com>
2023-01-21 15:58:47 -0600

    Initial commit

---
+++ lib/self_ref_sec_group-stack.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class SelfRefSecGroupStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+  }
+}
---
+++ test/self_ref_sec_group.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SelfRefSecGroup from '../lib/self_ref_sec_group-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/self_ref_sec_group-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SelfRefSecGroup.SelfRefSecGroupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
