437f8aad75810f511480004b44f3d2b446732a67
Ben Ellis <ben@element11.com>
2023-01-21 16:33:40 -0600

    Add example

---
+++ README.md
@@ -0,0 +1,3 @@
+# Self-referencing Security Group Example
+
+See the full write-up at [benellis.cloud](https://benellis.cloud/how-to-add-a-self-referencing-security-group-rule-via-the-aws-cdk).
--- lib/self_ref_sec_group-stack.ts
+++ lib/self_ref_sec_group-stack.ts
@@ -1,12 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
+import { IpAddresses, Port, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SelfRefSecGroupStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "vpc", {
+      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+      subnetConfiguration: [
+        {
+          name: "public-subnet",
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 20,
+        },
+      ],
+    })
 
+    const selfRefSg = new SecurityGroup(this, "SelfRefSg", {
+      vpc,
+      allowAllOutbound: true,
+    })
+
+    selfRefSg.addIngressRule(
+      selfRefSg,
+      Port.allTraffic(),
+      "allow local VPC traffic",
+    )
   }
 }
